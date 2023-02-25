d31680273f4f722a3c98d7229f375e0ccf5cdd83
Turan98 <48137040+Turan98@users.noreply.github.com>
2023-02-25 11:20:48 +0400

    test20

---
+++ array_min_max/Program.cs
@@ -0,0 +1,19 @@
+﻿int [] array  = new int [] {2, 6, -3, 4, 7, 0};
+int min = array[0];
+int max = array[0];
+for (int i = 0; i < array.Length; i++)
+{
+    if (min > array[i])
+    {
+        min = array[i];
+    
+    }
+
+    if (max < array[i])
+    {
+        max = array[i];
+    }
+}
+
+Console.WriteLine("Min elemet in array: " + min);
+Console.WriteLine("Max elemet in array: " + max);
